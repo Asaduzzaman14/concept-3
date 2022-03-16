a64f87cf0caf6ce059c83fc7a15c5885f7030801
Md Asaduzaman <asadasad9282@gmail.com>
2022-03-16 17:45:26 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <IncrimentDec></IncrimentDec>
     </div>
   );
 }
+
+function IncrimentDec() {
+
+  const [count, setCount] = useState([0])
+
+  incriment = () => setCount(count + 1)
+
+
+
+  return (
+    <div>
+      <h2>Incriment and Decrimant</h2>
+      <h1>Counter:{count}</h1>
+      <button onClick={incriment}>Incriment</button>
+      <button>Dicriment</button>
+    </div>
+  )
+}
+
+
+
 
 export default App;
